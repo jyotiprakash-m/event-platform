d8ee292f9b12d3c025866ff04ec836faad26a3fd
jyotiprakash-m <teachmeonline365@gmail.com>
2023-12-17 10:25:44 +0530

    complete app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,23 @@
-// import CategoryFilter from "@/components/shared/CategoryFilter";
-// import Collection from "@/components/shared/Collection";
-// import Search from "@/components/shared/Search";
+import CategoryFilter from "@/components/shared/CategoryFilter";
+import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
-// import { getAllEvents } from "@/lib/actions/event.actions";
-// import { SearchParamProps } from "@/types";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-// export default async function Home({ searchParams }: SearchParamProps) {
-export default async function Home() {
-  // const page = Number(searchParams?.page) || 1;
-  // const searchText = (searchParams?.query as string) || "";
-  // const category = (searchParams?.category as string) || "";
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
 
-  // const events = await getAllEvents({
-  //   query: searchText,
-  //   category,
-  //   page,
-  //   limit: 6,
-  // });
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6,
+  });
 
   return (
     <>
@@ -54,13 +53,13 @@ export default async function Home() {
         <h2 className="h2-bold">
           Trust by <br /> Thousands of Events
         </h2>
+
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          {/* <Search /> */}
-          Search
-          {/* <CategoryFilter /> */}
-          Category filter
+          <Search />
+          <CategoryFilter />
         </div>
-        {/* <Collection 
+
+        <Collection
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
@@ -68,8 +67,7 @@ export default async function Home() {
           limit={6}
           page={page}
           totalPages={events?.totalPages}
-        /> */}
-        Collection
+        />
       </section>
     </>
   );
